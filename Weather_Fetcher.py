ec3cc009554ec5dde2d5ec396b9766859a071779
Seb <sebcyde@hotmail.com>
2022-01-23 18:25:25 +0000

    Programme Finished

--- Weather_Fetcher.py
+++ Weather_Fetcher.py
@@ -1,4 +1,19 @@
 import requests
 
 API_KEY = "4b568e2bfd925854a38386e36bf9ac06"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    print(weather)
+    temperature = round(data['main']['temp'] - 273.15)
+
+    print('The weather is', temperature, 'celcius')
+else:
+    print("An error occured")
 
